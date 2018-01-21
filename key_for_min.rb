db99361ed28c5b94eaa02ff5844c26413a789f42
Brent  Ransom <sadumedusa@gmail.com>
2018-01-21 01:29:40 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,10 @@
 
 def key_for_min_value(name_hash)
   name_hash.collect do |name, val|
-    if val == 1 && 10
+    if val == 1 && val == 10
       return name
+    else
+      nil
     end
   end
 end
