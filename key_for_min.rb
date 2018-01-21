7ad0b0baa9f38d3b7fde1efe88b0740b2ae2badf
Brent  Ransom <sadumedusa@gmail.com>
2018-01-21 01:22:57 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash.collect do |value|
+    if value < 500 || 2 || 50 || 17
+      return
+    end
 end
